4496f090a84652093e46b9ad818404675fcffec1
leoloso <leo@getpop.org>
2025-09-10 22:10:25 +0800

    Deploy v14.0 to the WP.org repo step by step

--- src/Services/EndpointExecuters/GraphQLQueryResolutionEndpointExecuterInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace GatoGraphQL\GatoGraphQL\Services\EndpointExecuters;
-
-interface GraphQLQueryResolutionEndpointExecuterInterface extends GraphQLEndpointExecuterInterface
-{
-}
